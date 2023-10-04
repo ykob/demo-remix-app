ca7ed4c71f3d1e7d025dae9649c10f72ffa241dc
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-10-04 15:46:49 +0900

    load the single contact data dynamically.

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,18 +1,30 @@
-import { Form } from "@remix-run/react";
-import type { FunctionComponent } from "react";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
 
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import type { FunctionComponent } from "react";
 import type { ContactRecord } from "../data";
 
-export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+import { getContact } from "../data";
 
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+
+  const contact = await getContact(params.contactId);
+
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ contact });
+};
+
+export default function Contact() {
+  const { contact } = useLoaderData<typeof loader>();
+
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
   return (
     <div id="contact">
       <div>
