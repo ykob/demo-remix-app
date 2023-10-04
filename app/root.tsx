bfa0156d7b0b6c15bd0b42e7a210873c9242546b
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-10-04 15:46:02 +0900

    added action for create empty contact.

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ import {
   useLoaderData,
 } from "@remix-run/react";
 import appStylesHref from "./app.css";
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
@@ -21,6 +21,11 @@ export const links: LinksFunction = () => [
 export const loader = async () => {
   const contacts = await getContacts();
   return json({ contacts });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
 };
 
 export default function App() {
