ab9f8b2cb4abd9a88606b27b2dd2c8400813b654
ykob <info@tplh.net>
2024-02-12 20:53:32 +0900

    added api command for production start.

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 
 const app = express();
+const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+if (process.env.NODE_ENV === "production") {
+  app.listen(port, () => {
+    console.info(`Server is running on http://localhost:${port}/`);
+  });
+}
 
 export const viteNodeApp = app;
