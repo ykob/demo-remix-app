5c7c5dff0a9e23894f2a3ea5116f2577c66eecaf
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2024-02-13 09:38:38 +0900

    added prettier config.

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-organize-imports'],
+};
